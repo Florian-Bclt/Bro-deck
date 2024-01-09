0123e972d77a72c929ce7456c91ddbfc507d7842
Florian <bouclet.florian@hotmail.com>
2024-01-09 23:47:08 +0100

    edit searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './SearchBar.css'
+import { CiSearch } from "react-icons/ci";
 
 function SearchBar() {
   return (
-    <div>SearchBar</div>
+    <div className="app__searchbar">
+      <div className="app__searchbar-text">
+        <h2>Les cartes n'ont plus de secret</h2>
+      </div>
+      <div className="app__searchbar-input">
+        <CiSearch className='search-icon'/>
+        <input type="text" placeholder='Rechercher une carte' />
+        <button className='search-btn'>Go !</button>
+      </div>
+    </div>
   )
 }
 
